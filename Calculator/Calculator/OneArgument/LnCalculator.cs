d04adeacf3980eae2744909371fd54b5b0aa692e
DImon_162 <Dimdim_kz@mail.ru>
2016-05-20 14:39:44 +0600

    Argument 1

--- Calculator/Calculator/OneArgument/LnCalculator.cs
+++ Calculator/Calculator/OneArgument/LnCalculator.cs
@@ -2,8 +2,16 @@
 
 namespace Calculator.OneArgument
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class LnCalculator : ISingleCalculator
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="firstArgument"></param>
+        /// <returns></returns>
         public double Calculate(double firstArgument)
         {
             return Math.Log(firstArgument);
